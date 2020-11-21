1fdcede6ea8aeb814c6d85141ecddaf41f6ebae3
Joey3001 <josephnero111@gmail.com>
2020-11-20 16:15:48 -0800

    website commit

---
+++ src/js/ExchangeFetch.js
@@ -0,0 +1,40 @@
+import $ from 'jquery'
+export default class ExchangeFetch {
+
+  static async currencyCall() {
+    try { 
+      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      console.log(response);
+      if (!response.ok) {
+        throw Error(response.statusText); 
+      }
+      return response.json();
+    } 
+    catch(error) {
+      return error.message;
+    }
+  }
+
+  static getElements(response, money, currency) {
+    if (response.conversion_rates) {
+      if (response.conversion_rates[currency]) {
+        $('#output').text('The exchanged value is: ' + (parseFloat(money)*response.conversion_rates[currency]).toFixed(2));
+      }
+      else {
+        $('#output').text('That is not a currency supported by this application. Please try another currency');
+      }
+    }
+    else if (typeof(response) === 'object') {
+      $('#output').text(`There was the following error: ${response['error-type']}`)
+    }
+    else {
+      $('#output').text(`There was the following error: ${response}`)
+    }
+  }
+
+  static async makeApiCall(money, currency) {
+    const response = await ExchangeFetch.currencyCall();
+    ExchangeFetch.getElements(response, money, currency); 
+  }
+}
+
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import ExchangeFetch from './js/ExchangeFetch.js';
+
+
+$('#exchange').submit(function() {
+  event.preventDefault(); 
+  let money = $('#money').val(); 
+  let currency = $('#alternateCurrency').val();
+  ExchangeFetch.makeApiCall(money, currency);
+}) 
